1da621e3c3617b024737e0d2239d9b2333a5b982
sanjaraT <sanjaratunola@gmail.com>
2025-08-05 21:47:31 +0600

    Mthematical Operations

---
+++ MathOpr.py
@@ -0,0 +1,14 @@
+number_1 = 100
+number_2 = 63
+
+sum = number_1 + number_2
+diff = abs(number_1-number_2)
+mul = (number_1*number_2)
+div = (number_1/number_2)
+rem = (number_1%number_2)
+
+print("Sum : ", sum)
+print("Difference : ", diff)
+print("Multipliaction : ", mul)
+print("Division : ", div)
+print("Remiander : ", rem)
