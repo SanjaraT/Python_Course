76707c99e363a380cd88ceee072b79f8e437606b
sanjaraT <sanjaratunola@gmail.com>
2025-08-05 21:40:31 +0600

    First Program

---
+++ FirstProgram.py
@@ -0,0 +1,10 @@
+name = "Sanjara"
+age = 25
+email = "sanjaratunola@gmail.com"
+married = False
+
+print("My name is : ", name)
+print("I am ",age,"years old")
+print(type(email))
+print(type(age))
+print(type(married))
