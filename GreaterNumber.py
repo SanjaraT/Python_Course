61f99dea66976e80d6b62acb594d6e638d713086
sanjaraT <sanjaratunola@gmail.com>
2025-08-06 14:53:14 +0600

    Lecture1 Exercise4

---
+++ GreaterNumber.py
@@ -0,0 +1,10 @@
+print("--PROGRAM TO FIND OUT THE GREATER NUMBER--")
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+if (a>b):
+    print("True : the first number is greater than the second number.")
+elif (b>a):
+    print("False : the second number is greater than the first number.")
+else: 
+    print("None")
+
