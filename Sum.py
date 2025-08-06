4cfb374d541ccf7c0086c33dc5347bda967b7c66
sanjaraT <sanjaratunola@gmail.com>
2025-08-06 14:08:08 +0600

    Lecture1 Exercise1

---
+++ Sum.py
@@ -0,0 +1,6 @@
+
+print("--PROGRAM TO PRINT SUM OF TWO NUMBERS--")
+num1 = float(input("Enter first number : "))
+num2 = float(input("Enter second number : "))
+sum = num1 + num2 
+print("Sum of the two numbers is : ", sum)
