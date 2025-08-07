a2029c9097df9c1d27777be5132c3f01e86f8300
sanjaraT <sanjaratunola@gmail.com>
2025-08-07 20:51:39 +0600

    Lecture2 Exercise4

---
+++ OddEven.py
@@ -0,0 +1,6 @@
+print("--PROGRAM TO FIND IF A NUMBER IS ODD OR EVEN--")
+num = int(input("Enter an integer number : "))
+if (num % 2 == 0):
+    print("The number is even.")
+else:
+    print("The number is odd.")
