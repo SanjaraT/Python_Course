46506f6ac9bdf17c662e790e14d1950f92655cc4
sanjaraT <sanjaratunola@gmail.com>
2025-09-17 12:14:19 +0600

    Lecture3 Exercise1

---
+++ list1.py
@@ -0,0 +1,7 @@
+print("--PROGRAM TO PRINT FAV MOVIES --")
+
+movies = []
+movies.append(input("Enter first movie name: "))
+movies.append(input("Enter second movie name: "))
+movies.append(input("Enter third movie name: "))
+print(movies)
