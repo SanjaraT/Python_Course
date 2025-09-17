5447ac84332a4f63d3743527a07519a0642aaa8a
sanjaraT <sanjaratunola@gmail.com>
2025-09-17 12:33:55 +0600

    Lecture3 Exercise2

---
+++ Epalindrome.py
@@ -0,0 +1,9 @@
+print("--PROGRAM TO CHECKPLINDROME OR NOT--")
+sen = input("Enter a sentence separated by spaces: ")
+sen_list = sen.split()
+sen_list2 = sen_list.copy()
+sen_list2.reverse()
+if (sen_list2 == sen_list):
+    print("Palindrome")
+else:
+    print("Not Palindrome")
