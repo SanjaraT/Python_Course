439f557817ca74cbdbd38ca1f903cd4b338944ef
sanjaraT <sanjaratunola@gmail.com>
2025-11-29 20:28:28 +0600

    while Loop 2

---
+++ whiileLoop2.py
@@ -0,0 +1,6 @@
+print("PRINT THE MULTIPLICATION TABLE OF N")
+n = int(input("Enter the vlaue of n : "))
+i = 1 
+while i <=10:
+    print (n*i)
+    i += 1
--- whileloop1.py
+++ whileloop1.py
@@ -1,6 +1,5 @@
 print("PRINT NUMBERS FROM 1 TO 100")
-i = 1 
-while i <= 100:
+i = 100
+while i >= 1:
     print(i)
-    i += 1
-    
+    i -= 1
