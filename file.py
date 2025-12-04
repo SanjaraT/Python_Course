73f13302e7cd026346060c267586c4e326c18e2d
sanjaraT <sanjaratunola@gmail.com>
2025-12-04 12:42:43 +0600

    searching word

--- file.py
+++ file.py
@@ -1,9 +1,18 @@
 """ with open("pythonf.txt","w") as f:
     f.write("Hi everyone, i am sanjara\nWe are learning file I/O.\nusing java.\nI love programming in java. ") """
-with open("pythonf.txt","r") as f:
+""" with open("pythonf.txt","r") as f:
     data = f.read()
     new_data = data.replace("java","python")
     print(new_data)
 
 with open("pythonf.txt","w") as f:
-    data = f.write(new_data)
+    data = f.write(new_data) """
+def check_word():
+    word = input("Enter the word to be searched: ")
+    with open("pythonf.txt","r") as f:
+        data = f.read()
+        if (data.find(word) != -1):
+            print("Found")
+        else:
+            print("Not Found")
+check_word()
