697f7c75ba9e82980a02013fa952a28e78fd63c7
sanjaraT <sanjaratunola@gmail.com>
2025-12-04 12:36:16 +0600

    replacing word

--- file.py
+++ file.py
@@ -1,2 +1,9 @@
+""" with open("pythonf.txt","w") as f:
+    f.write("Hi everyone, i am sanjara\nWe are learning file I/O.\nusing java.\nI love programming in java. ") """
+with open("pythonf.txt","r") as f:
+    data = f.read()
+    new_data = data.replace("java","python")
+    print(new_data)
+
 with open("pythonf.txt","w") as f:
-    f.write("Hi everyone, i am sanjara\nWe are learning file I/O.\nusing java.\nI love programming in java. ")
+    data = f.write(new_data)
